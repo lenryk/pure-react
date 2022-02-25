e289cf431359c7717e27090dfd4b7d46afe1a3c2
lenryk <lenry101@gmail.com>
2022-02-25 22:18:00 +0000

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,134 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function HelloWorld() {
+  return (
+    <div>Hello World</div>
+  )
+}
+
+function Books() {
+  return (
+  <div className='book'>
+    <div className='title'>
+      The Title
+    </div>
+    <div className='author'>
+      The Author
+    </div>
+    <ul className='stats'>
+      <li className='rating'>
+        5 stars
+      </li>
+      <li className='isbn'>
+        12-345678-910
+      </li>
+    </ul>
+  </div>
+  )
+}
+
+function EmptyLineExamples() {
+  return (
+    <>
+      <div>
+        Newline
+        Test
+      </div>
+      <div>
+        Empty
+
+        Newlines
+
+        Here
+      </div>
+      <div>
+        &nbsp;Non-breaking
+        &nbsp;Spaces&nbsp;
+      </div>
+      <div>
+        Line1
+        {' '}
+        Line2
+      </div>
+    </>
+  )
+}
+
+// How to create a React element without components
+// React.createElement(
+//   type,
+//   [props],
+//   [...children]
+// )
+
+function BooksTwo() {
+  return (
+    React.createElement('div', {className: 'book'},
+    React.createElement('div', {className: 'title'}, 'The Title'),
+    React.createElement('div', {className: 'author'}, 'The Author'),
+    React.createElement('ul', {className: 'stats'},
+      React.createElement('li', {className: 'rating'}, 'The Title'),
+      React.createElement('li', {className: 'isbn'}, '12-345678-910')
+    )
+  )
+  )
+}
+
+function Greetings() {
+  let username = "root";
+  // let username = undefined;
+  // let username = null;
+  // let username = false;
+
+  return (
+    <>
+    {username ? `Hello ${username}` : "Not logged in"}
+    </>
+  )
+}
+
+function Table() {
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>Test</td>
+        </tr>
+        <tr>
+          <td>Test2</td>
+        </tr>
+        <tr>
+          <td>Test3</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+function TableModified() {
+  return (
+    <table>
+      <tbody>
+       <Table />
+       <Table />
+       <Table />
+      </tbody>
+    </table>
+  )
+}
+
+function Data() {
+  return (
+    <tr>
+      <td>Test</td>
+    </tr>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Table />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
