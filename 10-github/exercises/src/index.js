cf4fa452e0f634b957994c0f4c9431a739f46620
lenryk <lenry101@gmail.com>
2022-03-03 23:55:41 +0000

    added images to pinterest exercise

--- 10-github/exercises/src/index.js
+++ 10-github/exercises/src/index.js
@@ -138,7 +138,107 @@ const hackerData = [
   }
 ]
 
+// Pinterest
 
-ReactDOM.render(<HackerNewsList data={hackerData} />,
+function PinterestList({data}) {
+  return (
+    <div className="pinterestList">
+    {data.map(({title, user, image, pins, smallImageOne, smallImageTwo, smallImageThree}) => <PinterestBoard title={title} user={user} image={image} pins={pins} smallImageOne={smallImageOne} smallImageTwo={smallImageTwo} smallImageThree={smallImageThree} />)}
+    </div>
+  )
+}
+
+function PinterestBoard({title, user, image, pins, smallImageOne, smallImageTwo, smallImageThree}) {
+  return (
+    <div className="pinterestWrapper">
+      <div className="content">
+        <h4>{title}</h4>
+        <p>{user}</p>
+      </div>
+      <div className="imageWrapper">
+      <img src={image} alt="cover pic" className="coverPic" />
+        <p className="pinsWrapper">{pins}</p>
+      </div>
+      <div className="smallImages">
+        <img className="smallImage" src={smallImageOne} alt="small one" />
+        <img className="smallImage" src={smallImageTwo} alt="small two" />
+        <img className="smallImage" src={smallImageThree} alt="small three" />
+      </div>
+      <button>Follow</button>
+    </div>
+  )
+}
+
+const pinterestData = [
+  {
+    title: "DIY - Tips Tricks Ideas Repair",
+    user: "Aviation Explorer",
+    image: "https://cdn.lifehack.org/wp-content/uploads/2013/02/diy-projects-1024x768.jpeg",
+    pins: 4850,
+    smallImageOne: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/painting-a-jar-royalty-free-image-800402850-1532647880.jpg",
+    smallImageTwo: "https://www.pictureframesexpress.co.uk/blog/wp-content/uploads/2020/03/DIY-Crafts-for-your-Home-JPEG-e1585579765723.jpg",
+    smallImageThree: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/diy-wall-decor-san-giorgio-suites-famoso-012-1578599787.jpg",
+  },
+  {
+    title: "AVIATION",
+    user: "Aviation Explorer",
+    image: "https://compote.slate.com/images/222e0b84-f164-4fb1-90e7-d20bc27acd8c.jpg",
+    pins: 641,
+    smallImageOne: "https://www.rd.com/wp-content/uploads/2020/01/GettyImages-1131335393-e1580493890249-scaled.jpg",
+    smallImageTwo: "https://www.irmi.com/images/default-source/article-images/aviation/boeing-737.jpg",
+    smallImageThree: "https://cdn.luxatic.com/wp-content/uploads/2021/08/best-small-airplanes.jpg",
+  },
+  {
+    title: "Exceptional Avaition",
+    user: "Aviation Explorer",
+    image: "https://cdn.shopify.com/s/files/1/0061/7735/7891/articles/airplanes-actuators_17389e9d-f144-4f38-8d51-f8632a63c39c_1200x1200.jpg?v=1585138977",
+    pins: 209,
+    smallImageOne: "https://www.grunge.com/img/gallery/the-real-reason-airplanes-are-almost-always-painted-white/intro-1567189774.jpg",
+    smallImageTwo: "https://www.motherjones.com/wp-content/uploads/2021/05/20210518_zia_c218_005.jpeg",
+    smallImageThree: "https://www.grunge.com/img/gallery/the-real-reason-airplanes-are-almost-always-painted-white/intro-1567189774.jpg",
+  },
+  {
+    title: "AEROSPACE",
+    user: "Aviation Explorer",
+    image: "",
+    pins: 1495,
+    smallImageOne: "",
+    smallImageTwo: "",
+    smallImageThree: "",
+  },
+  {
+    title: "WHEN FLYING WAS WORT...",
+    user: "Aviation Explorer",
+    image: "",
+    pins: 209,
+    smallImageOne: "",
+    smallImageTwo: "",
+    smallImageThree: "",
+  },
+  {
+    title: "Aircraft Recycled Into Furni...",
+    user: "Aviation Explorer",
+    image: "",
+    pins: 75,
+    smallImageOne: "",
+    smallImageTwo: "",
+    smallImageThree: "",
+  }
+]
+
+// Internet Radio
+
+const radioData = 
+  {
+    large: ["Smooth Jazz", "Classic Rock", "Easy Listening", "Jazz", "Blues", "80s", "Rock", "Classical", "Country"],
+    big: ["Oldies", "Ambient", "House", "Top 40", "Chillout", "Lounge", "70s", "Trance", "Techno", "Reggae"],
+    medium: ["Pop", "Metal", "Deep House", "Hip Hop", "Greek", "Funk", "Instrumental", "Heavy Metal", "90s", "60s", "Drum And Bass", "Psytrance", "Soul", "Disco", "Meditation","Edm", "Salsa", "Bollywood", "50s", "Hindi"],
+    small: ["Rockability", "Christian", "Chill", "Electronic", "Rap", "Alternative", "Talk", "Big Band", "Swing", "New Age", "Dub", "Rnb", "Dancehall", "Downtemp", "Latin", "Psychedlic", "Anime", "Christmas", "Hardcore"]
+  }
+  
+
+
+
+ReactDOM.render(<PinterestList data={pinterestData} />,
   document.getElementById('root')
 );
